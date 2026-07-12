2631614e4831edece8b6032f75693dd5a39f532b
muhmmadshan786 <shanihunyar786003@gmail.com>
2026-07-13 03:36:03 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,15 @@
-from flask import Flask
+from flask import Flask, jsonify
+import requests
 
 app = Flask(__name__)
 
+API_KEY = "YAHAN_APNI_API_KEY"
+
 @app.route("/")
 def home():
-    return {
-        "status": "online",
-        "message": "Shan AI Signal API is running"
-    }
+    url = f"https://api.twelvedata.com/price?symbol=GBP/USD&apikey={API_KEY}"
+    data = requests.get(url).json()
+    return jsonify(data)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
