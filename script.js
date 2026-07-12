6fcf7597d4210405241b42d22ff8392ee769a9f9
muhmmadshan786 <shanihunyar786003@gmail.com>
2026-07-13 02:41:45 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
-function generateSignal() {
-    let signals = ["CALL 📈", "PUT 📉"];
-    let signal = signals[Math.floor(Math.random() * signals.length)];
-    let chance = Math.floor(Math.random() * 15) + 85;
+function generateSignal(){
 
-    document.getElementById("signal").innerHTML = signal;
-    document.getElementById("chance").innerHTML = chance + "% Accuracy";
+let signals = ["🟢 UP","🔴 DOWN"];
+let signal = signals[Math.floor(Math.random()*2)];
+
+let chance = Math.floor(Math.random()*11)+90;
+
+document.getElementById("signal").innerHTML = signal;
+document.getElementById("chance").innerHTML = "Accuracy: " + chance + "%";
+
+if(signal=="🟢 UP"){
+document.getElementById("signal").style.color="lime";
+}else{
+document.getElementById("signal").style.color="red";
+}
+
 }
